de9acada3b698e6bfe2a2ab81c90c63e15bb570b
Akinlowo Olumide Adeola <adeolaolumide9@gmail.com>
2023-09-14 18:09:13 +0100

    Update src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,9 @@ import Footer from './components/Footer.vue';
 export default {
   data() {
     return {
-      baseURL: 'https://limitless-lake-55070.herokuapp.com/',
-      //baseURL: "http://localhost:8080/",
+      // baseURL: 'https://limitless-lake-55070.herokuapp.com/',
+      // baseURL: "http://localhost:8080/",
+      baseURL: "http://18.216.67.25:8443/",
       products: null,
       categories: null,
       key: 0,
